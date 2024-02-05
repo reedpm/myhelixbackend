70654bd3db16b272d804ec7fd6995b33b91389b1
Guadalupe Aliseda-Canton <alisedac@usc.edu>
2024-02-05 11:54:33 -0800

    add posts to profile screen

--- frontend/ProfileScreen.js
+++ frontend/ProfileScreen.js
@@ -1,7 +1,8 @@
 // ProfileScreen.js
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 import { dbURI } from './App';
+import Post from './Post';
 
 const ProfileScreen = ({ route }) => {
   const { personalProfile, publicProfile } = route.params;
@@ -22,24 +23,48 @@ const ProfileScreen = ({ route }) => {
           console.error('Failed to fetch profile data');
           return;
         }
-
+  
         const data = await response.json();
-        setProfileData(data.data);
-        console.log("profile data: ", profileData);
+  
+        // Fetch user posts
+        const postsResponse = await fetch(dbURI + `posts/getPostsByProfileID/${currentProfileID}`);
+        if (!postsResponse.ok) {
+          console.error('Failed to fetch posts data');
+          return;
+        }
+  
+        const postsData = await postsResponse.json();
+  
+        // Update profileData state with posts
+        setProfileData({
+          ...data.data,
+          posts: postsData.data,
+        });
+  
       } catch (error) {
         console.error('Error during profile data fetch:', error);
       }
     };
-
+  
     fetchProfileData();
   }, [currentProfileID]);
 
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>{profileData?.type}</Text>
-        <Text style={styles.label}>User: {profileData?.user}</Text>
-        <Text style={styles.label}>Display Name: {profileData?.displayName}</Text>
-        <Button title="Change profile" onPress={changeCurrentProfileID} />
+        { profileData ? (
+            <>
+            <Text style={styles.title}>{profileData?.type}</Text>
+            <Text style={styles.label}>User: {profileData?.user}</Text>
+            <Text style={styles.label}>Display Name: {profileData?.displayName}</Text>
+            <Button title="Change profile" onPress={changeCurrentProfileID} />
+            <FlatList
+                data={profileData.posts}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item }) => <Post post={item} />}
+            />
+            </>
+        ) : null
+        }
         {/* Add more components to display other profile information as needed */}
     </View>
   );
