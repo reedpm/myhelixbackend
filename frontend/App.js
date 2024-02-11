4e30421b4fa2dd35f591240d4bb2b94779d484f3
Guadalupe Aliseda-Canton <alisedac@usc.edu>
2024-02-10 16:18:19 -0800

    update build rules

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import HomeScreen from './screens/HomeScreen';
