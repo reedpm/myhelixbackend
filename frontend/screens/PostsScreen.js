239628fae40e8db6baa2fc91b9bae1950f58b257
nguyenh94 <huongbng@usc.edu>
2024-02-18 15:32:08 -0800

    added sample posts

--- frontend/screens/PostsScreen.js
+++ frontend/screens/PostsScreen.js
@@ -5,17 +5,25 @@ import { useNavigation } from '@react-navigation/native';
 
 const PostsScreen = () => {
 
-    const post = {
+    const post1 = {
         postBody: "This is the content of the post.",
         createdBy: "John Doe",
         createDate: "2024-02-17",
         category: "Technology",
     };
 
+    const post2 = {
+        postBody: "aldkaf;alskdjfaldflkasjfe;slkajdflaksejfas. asldkfjealksjdfaelskfjesflksdf. laksdjfleksajlfjasdfe.",
+        createdBy: "Huong Nguyen",
+        createdDate: "2024-02-18",
+        category: "Random",
+    }
+
     return (
         <View style={styles.container}>
             {/* Passing the post object as the post prop to the Post component */}
-            <Posts post={post} />
+            <Posts post={post1} />
+            <Posts post={post2} />
         </View>
 
     );
