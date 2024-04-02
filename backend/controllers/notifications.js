8468fa5c51080ac1dee1e0bb4c46f612b344d854
Guadalupe Aliseda-Canton <alisedac@usc.edu>
2024-04-01 18:09:03 -0700

    notifications model, controller and routes added

---
+++ backend/controllers/notifications.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Notification = require("../models/notifications");
+const Profile = require("../models/profile");
+
+exports.addNotification = async (req, res, next) => {
+    try{
+        // get the profile id of the user that is generating the notification
+        const notificationSender = await Profile.findById(req.body.senderProfileID);
+
+        // get the profile id of the user that the notification is being sent to
+        const notificationRecipient = await Profile.findById(req.body.recipientProfileID);
+
+        // Create new Notification Document
+        const notification = new Notification(
+            {
+                _id: new mongoose.Types.ObjectId(),
+                notificationType: req.body.notificationType,
+                sender: notificationSender._id,
+                recipient: notificationRecipient._id,
+                read: false,
+            }
+        );
+        console.log(notificationSender);
+        console.log(notificationRecipient);
+        // Save the notification into the database
+        await notification.save();
+
+        res.status(200).send("Succesfully created a new notification");
+    }
+    catch(err){
+        next(err);
+    }
+}
+
+exports.getNotificationsForProfile = async (req, res, next) => {
+    try{
+        Notification.find({ recipient: req.params.profileID }).exec()
+        .then(data => {
+            // If data is found, send it back
+            console.log(data);
+            res.status(200).send({ data: data });
+        })
+        .catch(err => {
+            // If an error occurs, send an error response
+            res.status(403).send({ data: err.message });
+        });
+    }
+    catch(err){
+        console.log("error");
+        next(err);
+    }
+}
---
+++ backend/models/notifications.js
@@ -0,0 +1,59 @@
+/**
+ * Comment is for swagger documentation / defining the request schema
+ * @swagger 
+ * components:
+ *   schemas:
+ *     Notifications:
+ *       type: object
+ *       required:
+ *         - _id
+ *         - notificationType
+ *         - sender
+ *         - recipient
+ *       properties:
+ *         _id:
+ *           type: object
+ *           description: The MongoDB ID of the notification
+ *         notificationType:
+ *           type: string
+ *           description: The descriptor of the type of notification => Includes an enum of values to choose from
+ *         sender:
+ *           type: object
+ *           description: The profile ID of the profile that caused the notification
+ *         recipient:
+ *           type: array
+ *           description: The profile ID of the profile that the notification is sent to
+ */
+
+// This is the Conversation schema -> Direct Messages
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    notificationType: {
+        type: String,
+        required: true,
+        enum: ['FOLLOW', 'MESSAGE', 'LIKE', 'COMMENT']
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Profiles",
+        required: true,
+    },
+    recipient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Profiles",
+        required: true
+    },
+    read: {
+        type: mongoose.Schema.Types.Boolean,
+        required: true,
+    }
+    /** Convenience vars */
+}, {collection: "Notifications"});
+
+module.exports = mongoose.model("Notifications", notificationSchema);
+// Structure of Schema is further outlined in the Documentation
