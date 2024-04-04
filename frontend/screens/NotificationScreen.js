42525f6a666ae7fdadd683d9ebe9f05ae97e686a
Guadalupe Aliseda-Canton <alisedac@usc.edu>
2024-04-03 17:28:06 -0700

    clean up and add notifications when necessary

--- frontend/screens/NotificationScreen.js
+++ frontend/screens/NotificationScreen.js
@@ -5,42 +5,10 @@ import {
   View,
   SectionList,
 } from 'react-native';
-import {Divider, Avatar, Badge, ListItem} from '@rneui/themed';
-import {customFonts} from '../CustomFonts';
+import {Divider} from '@rneui/themed';
 import {fonts} from '../styles';
-import PropTypes from 'prop-types';
 import {useGlobalContext, dbURI} from '../GlobalContext';
-
-
-const NotificationItem = ({notification}) => {
-  customFonts();
-  const notificationMessages = {
-    'COMMENT': 'commented on your post.',
-    'LIKE': 'liked your post.',
-    'FOLLOW': 'followed you.',
-    'MESSAGE': 'messaged you.',
-  };
-  const styles = StyleSheet.create({
-    notification: {
-      fontWeight: 'bold',
-    },
-  });
-
-  return (
-    <ListItem bottomDivider>
-      <Avatar rounded source={{uri: notification.sender.profileImage ?? 'https://reactnative.dev/img/tiny_logo.png'}} />
-      <ListItem.Content>
-        <ListItem.Title style={styles.notification}>
-          {
-            notification.sender.displayName + ' ' +
-            notificationMessages[notification.type]
-          }
-        </ListItem.Title>
-      </ListItem.Content>
-      {!notification.read && <Badge status="error" />}
-    </ListItem>
-  );
-};
+import Notification from '../components/Notification';
 
 const NotificationsScreen = () => {
   const [notifications, setNotifications] = useState([]);
@@ -90,45 +58,20 @@ const NotificationsScreen = () => {
     <View style={styles.container}>
       {notifications ? (
       <SectionList
-        sections={[{title: 'Notifications', data: notifications || []}]}
+        sections={[{data: notifications || []}]}
         keyExtractor={(item, index) => item._id + index}
-        renderItem={({item}) => <NotificationItem notification={item} />}
+        renderItem={({item}) => <Notification notification={item} />}
         renderSectionHeader={({section: {title}}) => (
           <Text style={styles.sectionHeader}>{title}</Text>
         )}
         ItemSeparatorComponent={() => <Divider />}
       />
     ) : (
-      <Text>No notifications found</Text>
+      <Text>You have no notifications!</Text>
     )}
     </View>
   );
 };
 
-NotificationItem.propTypes = {
-  notification: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    type: PropTypes.oneOf(['COMMENT', 'LIKE', 'FOLLOW', 'MESSAGE']).isRequired,
-    sender: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      user: PropTypes.string.isRequired,
-      displayName: PropTypes.string.isRequired,
-      profileImage: PropTypes.string.isRequired,
-      bio: PropTypes.string.isRequired,
-      followers: PropTypes.array.isRequired,
-      following: PropTypes.array.isRequired,
-      conversations: PropTypes.array.isRequired,
-      posts: PropTypes.array.isRequired,
-      pages: PropTypes.array.isRequired,
-      incomingRequests: PropTypes.array.isRequired,
-      outgoingRequests: PropTypes.array.isRequired,
-      __v: PropTypes.number.isRequired,
-    }).isRequired,
-    recipient: PropTypes.string.isRequired,
-    read: PropTypes.bool.isRequired,
-    __v: PropTypes.number.isRequired,
-  }).isRequired,
-};
 
 export default NotificationsScreen;
