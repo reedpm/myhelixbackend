d1be4ee19b54496323455ed5ac2f8d47fd46717e
Guadalupe Aliseda-Canton <alisedac@usc.edu>
2024-04-14 17:36:38 -0700

    group notifications based on dates

--- frontend/screens/NotificationScreen.js
+++ frontend/screens/NotificationScreen.js
@@ -12,8 +12,40 @@ import Notification from '../components/Notification';
 
 const NotificationsScreen = () => {
   const [notifications, setNotifications] = useState([]);
+  const [notificationSections, setNotificationSections] = useState([]);
   const {currentProfileID} = useGlobalContext();
 
+  const groupNotificationsByDate = (notifications) => {
+    const groupedNotifications = {
+      'Today': [],
+      'Last Week': [],
+      'Older': [],
+    };
+
+    const today = new Date();
+    const lastWeek = new Date(today);
+    lastWeek.setDate(lastWeek.getDate() - 7);
+
+    notifications.forEach((notification) => {
+      const createDate = new Date(notification.createDate);
+
+      if (createDate.toDateString() === today.toDateString()) {
+        groupedNotifications['Today'].push(notification);
+      } else if (createDate > lastWeek) {
+        groupedNotifications['Last Week'].push(notification);
+      } else {
+        groupedNotifications['Older'].push(notification);
+      }
+    });
+
+    // Convert grouped notifications into sections
+    const sections = Object.keys(groupedNotifications).map((category) => ({
+      title: category,
+      data: groupedNotifications[category],
+    }));
+
+    return sections;
+  };
 
   const fetchNotifications = async () => {
     try {
@@ -30,6 +62,8 @@ const NotificationsScreen = () => {
       const notificationsData = await notificationsResponse.json();
 
       setNotifications(notificationsData.data);
+      const sections = groupNotificationsByDate(notificationsData.data);
+      setNotificationSections(sections);
     } catch (error) {
       console.error('Error during notifications fetch:', error);
     }
@@ -58,7 +92,9 @@ const NotificationsScreen = () => {
     <View style={styles.container}>
       {notifications ? (
       <SectionList
-        sections={[{data: notifications || []}]}
+        sections={
+          notificationSections.filter((section) => section.data.length > 0)
+        }
         keyExtractor={(item, index) => item._id + index}
         renderItem={({item}) => <Notification notification={item} />}
         renderSectionHeader={({section: {title}}) => (
